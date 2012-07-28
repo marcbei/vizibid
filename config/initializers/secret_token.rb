fb892930da3a144f33c87d27aa758fa1ca1bb090
Marc Beitchman <mbeitchman@gmail.com>
2012-07-28 16:11:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vizibid::Application.config.secret_token = '2c14425056aa8908040e2e14559db9ce5ef6dd7072e07457cefc708402c95eeafb9b4e868ee88015f3d7463ee14cebdd028ac55b3ae4d1ba2e5297660c51a0ad'
