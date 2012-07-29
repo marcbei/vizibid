36a20ec5b6214bf7f47a2b061f53a3c79cc5c4d2
Marc Beitchman <mbeitchman@gmail.com>
2012-07-28 22:39:37 -0700

    Add sign-up

--- db/migrate/20120729015051_add_index_to_users_email.rb
+++ db/migrate/20120729015051_add_index_to_users_email.rb
@@ -1,14 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration
   def change
   	add_index :users, :email, unique: true
-
-  	before_save { |user| user.email = email.downcase }
-
-  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  	validates :name,  presence: true, length: { maximum: 50 }
-  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-
   end
 end
