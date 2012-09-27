800bee31fc43c799a33c3816b56b9daa59bebb99
Marc Beitchman <mbeitchman@gmail.com>
2012-09-26 23:08:56 -0700

    Mailer updates

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,12 @@
 class Mailer < ActionMailer::Base
-  default from: "marc@vizibid.com"
+  default from: "info@vizibid.com"
 
   def feedback_email(user, subject, message)
   	@user = user
   	@subject = subject
   	@message = message
 
-  	mail(:to => "marc@vizibid.com", :subject => "New Feedback Received")
+  	mail(:to => "marc@vizibid.com, forrest@vizibid.com", :subject => "New Feedback Received")
 
   end
 
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "vizivid.com",
-  :user_name            => "marc@vizibid.com",
-  :password             => "sub81mit",
+  :domain               => "vizibid.com",
+  :user_name            => "info@vizibid.com",
+  :password             => ENV['INFO_ACCOUNT_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
