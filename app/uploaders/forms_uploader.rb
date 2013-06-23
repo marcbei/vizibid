d1aa21a30700903e369fc114cc22efe88d709121
Marc Beitchman <marc@vizibid.com>
2013-06-22 18:55:05 -0700

    Update error pages and change path for forms on aws

--- app/uploaders/forms_uploader.rb
+++ app/uploaders/forms_uploader.rb
@@ -17,7 +17,7 @@ class FormsUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.id}"
+    "forms/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
