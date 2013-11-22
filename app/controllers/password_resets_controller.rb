f1e42f034b393697581116780e63a3d972df5f28
Marc Beitchman <marc@vizibid.com>
2013-11-21 22:29:45 -0800

    bug fix and make password reset a modal

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,8 @@ class PasswordResetsController < ApplicationController
 	  user = User.find_by_email(params[:email])
 	  if user != nil
 	  	user.send_password_reset 
-	  	redirect_to root_path, :notice => "Email sent with password reset instructions."
+	  	flash[:success] = "Email sent with password reset instructions."
+	  	redirect_to root_path
 	  else
 	  	redirect_to root_path
 	  end
