ddd35b02f4364be2ed545b5db6c7d663bf42867f
Forrest Carlson <forrest@vizibid.com>
2013-12-02 14:54:59 -0800

    minor adjustments to copy and layout

--- app/controllers/user_feedbacks_controller.rb
+++ app/controllers/user_feedbacks_controller.rb
@@ -7,11 +7,11 @@ class UserFeedbacksController < ApplicationController
 			:comment => params[:user_feedback][:comment], :user_id => current_user.id)
 
 		if @userfeedback.save
-			flash[:success] = "Feedback submitted. Thank you."
+			flash[:success] = "We received your feedback. Thank you."
 			Mailer.delay.feedback_email(current_user, @userfeedback.subject, @userfeedback.comment)
 			redirect_to root_path
 		else
-			flash[:error] = "Unable to submit your feedback."
+			flash[:error] = "Unable to submit your feedback. You can email feedback to support@vizibid.com"
 			redirect_to root_path
 		end
 	end
