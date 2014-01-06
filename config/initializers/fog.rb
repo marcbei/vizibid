e23c81d32cd339b4407d25f950678ee3f4a51a23
Marc Beitchman <marc@vizibid.com>
2014-01-05 18:00:48 -0800

    Update fog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,9 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
   	:aws_secret_access_key  => ENV['AWS_SECRET_KEY'],       # required
   }
-  config.fog_directory  = ENV['AWS_BUCKET']                     # required
-  config.fog_host       = 'https://s3.amazonaws.com'            # optional, defaults to nil
+  config.fog_directory  = ENV['AWS_BUCKET']                     
   config.fog_public     = false          
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 end
