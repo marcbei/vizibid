5f119781947787969cc3374c4725db3f8a21031a
Marc Beitchman <marc@vizibid.com>
2014-01-05 23:34:41 -0800

    fog settings changes

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,6 +4,8 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
   	:aws_secret_access_key  => ENV['AWS_SECRET_KEY'],       # required
   }
+  config.storage = :fog
   config.fog_directory  = ENV['AWS_BUCKET']                     
-  config.fog_public     = false          
+  config.fog_public     = false 
+  config.cache_dir = "#{Rails.root}/tmp/uploads"            
 end
